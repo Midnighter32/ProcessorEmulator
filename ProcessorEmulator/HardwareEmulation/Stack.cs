7d4522fabe2365ac571c1b79539bc9ac3ed5cbc5
Vasilev Viktor <deleteduserc2c67505@gmail.com>
2022-09-01 09:01:08 +0300

    [Add] Stack

---
+++ ProcessorEmulator/HardwareEmulation/Stack.cs
@@ -0,0 +1,38 @@
+﻿using ProcessorEmulator.HardwareEmulation.Base;
+
+namespace ProcessorEmulator.HardwareEmulation
+{
+    class Stack : IHardware
+    {
+        private Stack()
+        {
+        }
+
+        Stack<int> _stack = new Stack<int>(16384);
+
+        public void Dump()
+        {
+            foreach (var item in _stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void Read(int ind, out int value)
+        {
+            if (_stack.Count != 0)
+                value = _stack.Pop();
+            else value = 0;
+        }
+
+        public void Write(int ind, int value)
+        {
+            _stack.Push(value);
+        }
+
+        public void Pop(out int value) => Read(-1, out value);
+        public void Push(int value) => Write(-1, value);
+
+        public static Stack Instance = new Stack();
+    }
+}
